718111e64e8cba6ae8d2776585cd747faeed3c4e
EyalSinay <eyalsinay@gmail.com>
2023-02-19 18:30:22 +0200

    update task status

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -9,8 +9,18 @@ import { TaskService } from 'src/app/services/task.service';
 })
 export class TaskComponent {
   @Input() task: Task = { _id: '', text: '', done: false, isOnEdit: false };
+  statusTaskValue: boolean = false;
 
   constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.statusTaskValue = this.task.done;
+  }
+
+  taskStatusChange() {
+    this.task.done = this.statusTaskValue;
+    this.taskService.updateTask(this.task);
+  }
 
   deleteTask(taskId: string) {
     this.taskService.deleteTask(taskId);
