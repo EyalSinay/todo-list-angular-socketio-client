8bcbe38a5a4c8ee0750cb412e18b161021973376
EyalSinay <eyalsinay@gmail.com>
2023-02-19 19:03:38 +0200

    edit task text

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -10,11 +10,33 @@ import { TaskService } from 'src/app/services/task.service';
 export class TaskComponent {
   @Input() task: Task = { _id: '', text: '', done: false, isOnEdit: false };
   statusTaskValue: boolean = false;
+  newTaskText: string = '';
 
   constructor(private taskService: TaskService) {}
 
   ngOnInit(): void {
     this.statusTaskValue = this.task.done;
+    this.newTaskText = this.task.text;
+  }
+
+  onEditClick() {
+    this.task.isOnEdit = true;
+    this.taskService.updateTask(this.task);
+  }
+
+  saveNewTaskText() {
+    if (this.newTaskText.length > 0) {
+      this.task.text = this.newTaskText;
+      this.task.isOnEdit = false;
+      this.taskService.updateTask(this.task);
+    } else {
+      alert('Please insert text first');
+    }
+  }
+
+  cancelEdit() {
+    this.task.isOnEdit = false;
+    this.taskService.updateTask(this.task);
   }
 
   taskStatusChange() {
